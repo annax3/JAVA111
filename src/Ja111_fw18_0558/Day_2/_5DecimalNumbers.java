c26f1dcfed185fabc95765b047fb9d2d7d224d64
Prashant Anand <906120anand@gmail.com>
2022-09-08 00:11:26 +0530

    "JAVA_Type_Casting_updated"

---
+++ src/Ja111_fw18_0558/Day_2/_5DecimalNumbers.java
@@ -0,0 +1,18 @@
+package Ja111_fw18_0558.Day_2;
+
+public class _5DecimalNumbers {
+    public static  void main(String[] args){
+        //default data type for decimal nums is double
+        //byte<int
+        //float < double
+        double d =5.35;
+        //casting : 2ways
+        //1st way
+        float f=(float)5.35;
+        //2nd way : shortcut
+        float g=5.35f;
+        System.out.println(f);
+        System.out.println(g);
+
+    }
+}
---
+++ src/Ja111_fw18_0558/Day_2/_6Statements.java
@@ -0,0 +1,7 @@
+package Ja111_fw18_0558.Day_2;
+
+public class _6Statements {
+    public static  void main(String[] args){
+
+    }
+}
