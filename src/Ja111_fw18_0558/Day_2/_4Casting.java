d60e6f734cd493441a1caec752633bc6156d08f1
Prashant Anand <906120anand@gmail.com>
2022-09-07 23:53:37 +0530

    "JAVA_Type_Casting_updated"

--- src/Ja111_fw18_0558/Day_2/_4Casting.java
+++ src/Ja111_fw18_0558/Day_2/_4Casting.java
@@ -9,5 +9,9 @@ public class _4Casting {
         int i=10;
         byte b = (byte) i;//explicit type casting
         System.out.println(b);
+        byte b1=20;
+        byte b2=25;
+        byte b3=(byte)(b1+b2);
+        System.out.println(b3);
     }
 }
